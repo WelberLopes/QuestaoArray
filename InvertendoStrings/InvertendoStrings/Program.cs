39e6cc19a1ae694f055e219f780893e9128d8f64
WelberLopes <deunatelhacontato01@gmail.com>
2024-03-07 19:57:35 -0300

    no message

---
+++ InvertendoStrings/InvertendoStrings/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace QuetoesEstagio {
+    class Program {
+        public static void Main(string[] args) {
+
+            string _entradaUser = Console.ReadLine();
+
+
+            char[] _arr = _entradaUser.ToCharArray();
+
+            int i;
+            int _tamanhoArray = _arr.Length;
+
+            for (i = 0; i < _arr.Length; i++) {
+
+
+                // Console.WriteLine(" {0}: {1}", i, _arr[i]);
+
+               // Console.Write("{0}", _arr[i]);
+
+           }
+            
+            while (i <= _arr.Length && _tamanhoArray >0) {
+                Console.Write(_arr[_tamanhoArray - 1]);
+
+                _tamanhoArray = _tamanhoArray - 1;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
+    
+
